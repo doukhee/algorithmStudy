d1819de5869314864f4b7a10d6fe0fa794679a07
duckhee <fain9301@yahoo.com>
2020-07-01 15:12:09 +0900

    first commit

---
+++ books/algorithm-solution-stragey/chapter01/02.manyPrintf/manyPrintf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Hello World \r\n");
+    printf("Merry Christmas\r\n");
+    printf("Happy New Year\r\n");
+
+    printf("Hello World \r\n");
+    printf("Merry Christmas\r\n");
+    printf("Happy New Year\r\n");
+
+    printf("Hello World \r\n");
+    printf("Merry Christmas\r\n");
+    printf("Happy New Year\r\n");
+    return 0;
+}
---
+++ books/algorithm-solution-stragey/chapter01/03.forProgram/forprogram.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    
+    int i;
+
+    for(i = 0; i < 3; i++){
+        printf("Hello World \r\n");
+        printf("Merry Christmas\r\n");
+        printf("Happy New year\r\n");
+    }
+    return 0;
+}
